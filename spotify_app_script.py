1533f1522c51aa0b7cd904bd84a5d8156b44c81d
sqrx-mckl <martinez.mickael1@gmail.com>
2020-04-06 01:49:21 +0200

    WIP: cluster progress, plotly issues separated the liked songs analysis tested some data visualization issues with plotly and events callback

--- spotify_app_script.py
+++ spotify_app_script.py
@@ -38,60 +38,6 @@ sp_adapter = lib.adapter_spotipy_api(
 
 sp_adapter.open_session()
 
-#%% [markdown]
-# As an example I can retrieve my top artists list and do some plots of it.
-
-#%% Query / Request
-top_artists = lib.normalize_request(
-    sp_adapter.sp.current_user_top_artists(limit=20)
-)
-
-#%% Plot
-
-display(top_artists[['name', 'popularity', 'followers.total']])
-
-top_artists_header = top_artists.name.to_frame().apply(
-    lambda x: f'{x.name}: {x.values[0]}', axis=1
-)
-
-top_artists[['popularity', 'followers.total']]\
-    .assign(top=top_artists_header)\
-    .plot(x='top',
-          kind='bar',
-          secondary_y='followers.total')
-
-top_artists[['popularity', 'followers.total']]\
-    .assign(top=top_artists_header)\
-    .plot(x='top',
-          kind='barh',
-          subplots=True,
-          layout=(1,2),
-          sharey=True,
-          sharex=False)
-
-# %%
-
-df_toptracks = lib.normalize_request(
-    sp_adapter.sp.current_user_top_tracks(limit=50)
-).pipe(lib.enrich_audiofeature, col='id')
-
-# %%
-
-genre_toptracks = lib.facade_enrich_artist_genre(
-    artists=df_toptracks['artists.0.id'],
-    sp=sp_adapter.sp
-)
-
-#%%
-genre_toptracks.cluster_genre(method='weighted',
-                              fit=True,
-                              verbose=True)
-
-#%%
-
-genre_toptracks.test_supergenre_distance()
-genre_toptracks.test_supergenre_maxclust()
-
 #%%
 
 df_likedsong:pd.DataFrame = lib.normalize_request(
@@ -99,6 +45,10 @@ df_likedsong:pd.DataFrame = lib.normalize_request(
 ).pipe(lib.enrich_audiofeature, col='track.id')
 
 df_likedsong.to_csv(Path('private', 'likedsongs.csv'))
+
+#%%
+
+df_likedsong:pd.DataFrame = pd.read_csv(Path('private', 'likedsongs.csv'))
 
 df_likedsong['added_at'] = pd.to_datetime(df_likedsong['added_at'])
 
@@ -108,6 +58,8 @@ df_likedsong['added_at']\
             .count()\
             .plot()\
             .set_title('How many songs I liked per day')
+
+df_likedsong = df_likedsong.drop('Unnamed: 0', axis=1)
 
 #%%
 
@@ -144,15 +96,31 @@ genre_likedsong.test_supergenre_maxclust()
 
 # %%
 
-# dill.dump_session(Path('private', 'session_dump_before_plot'))
-
-# %%
-
 genre_likedsong.cluster_genre_transform_auto(verbose=True)
+
+#%%
+
+import hdbscan
+
+mdl_hdbscan = hdbscan.HDBSCAN(
+    min_cluster_size=2,
+    cluster_selection_method='eom',
+    cluster_selection_epsilon=0.5,
+    min_samples=5,
+    metric='yule'
+)
+
+cluster_hdbscan = mdl_hdbscan.fit_predict(
+    genre_likedsong.df_supergenre.transpose()
+)
+
+display(genre_likedsong._get_group_list(cluster=cluster_hdbscan,
+                                        df_genre=genre_likedsong.df_supergenre))
 
 # %%
 df_likedsong_enrich = genre_likedsong.enrich_df(df_likedsong)
-display(df_likedsong_enrich.sample(20))
+display(len(df_likedsong_enrich))
+display(df_likedsong_enrich.sample(5))
 
 # %%
 
@@ -160,11 +128,12 @@ df_likedsong_enrich.to_csv(Path('private', 'likedsongs_enrich.csv'))
 
 # %%
 
-df_likedsong_enrich = pd.read_csv(Path('private', 'likedsongs_enrich.csv'))
+df_likedsong_enrich = pd.read_csv(Path('private', 'likedsongs_enrich.csv'),
+                                  index_col=0)
 
 #%%
 
-df = df_likedsong_enrich[[
+df:pd.DataFrame = df_likedsong_enrich[[
     'added_at',
     'track.album.id',
     'track.album.images.0.height',
@@ -198,9 +167,16 @@ df = df_likedsong_enrich[[
     'track.artists.0.supergenres'
 ]]
 
+#%%
 df.to_csv(Path('private', 'df_analysis.csv'))
 
-df:pd.DataFrame = pd.read_csv(Path('private', 'df_analysis.csv'))
+#%%
+df:pd.DataFrame = pd.read_csv(Path('private', 'df_analysis.csv'),
+                                   index_col=0)
+
+import yaml
+col_to_convert = ['track.artists.0.genres', 'track.artists.0.supergenres']
+df[col_to_convert] = df[col_to_convert].applymap(yaml.safe_load)
 
 # %%
 
@@ -215,9 +191,123 @@ for k, col in enumerate(df_num):
 df_num.hist(figsize=(20,14))
 
 #%%
+
+from sklearn.manifold import TSNE
+import umap
+import hdbscan
+from sklearn.neighbors import DistanceMetric
+
+def hdbscan_fit_transform(min_cluster_size, df):
+    mdl = hdbscan.HDBSCAN(
+        min_cluster_size=20,
+        metric='mahalanobis',
+        V=df.cov(),
+        gen_min_span_tree=True)
+    
+    df_output = mdl.fit_predict(df)
+    # mdl.minimum_spanning_tree_.plot()
+    # mdl.single_linkage_tree_.plot(cmap='viridis', colorbar=True)
+    return df_output
+
+df_analysis =  df[[
+    'track.id.danceability',
+    'track.id.energy',
+    'track.id.key',
+    'track.id.loudness',
+    'track.id.mode',
+    'track.id.speechiness',
+    'track.id.acousticness',
+    'track.id.instrumentalness',
+    'track.id.liveness',
+    'track.id.valence',
+    'track.id.tempo',
+    'track.id.time_signature',
+    'track.id',
+]].set_index('track.id')
+
+# mdl_tsne = TSNE(metric=DistanceMetric.get_metric('mahalanobis',
+#                                                 V=df_analysis.cov()))\
+#                 .fit_transform(df_analysis)
+
+mdl_tsne = TSNE(perplexity=30).fit_transform(df_analysis)
+
+mdl_umap = umap.UMAP(n_neighbors=20, metric='mahalanobis')\
+                .fit_transform(df_analysis)
+
+
+df['tsne_x'] = mdl_tsne[:,0]
+df['tsne_y'] = mdl_tsne[:,1]
+
+df['clusters_tsne'] = hdbscan.HDBSCAN(min_cluster_size=20)\
+                             .fit_predict(df[['tsne_x', 'tsne_y']])
+plt.figure()
+df['clusters_tsne'].hist().set_title('tsne')
+
+df['clusters_umap'] = hdbscan.HDBSCAN(min_cluster_size=20)\
+                             .fit_predict(df[['umap_x', 'umap_y']])
+plt.figure()
+df['clusters_umap'].hist().set_title('umap')
+
+#%%
+
+df['genres_str'] = df['track.artists.0.genres'].apply(
+    lambda x: ' '.join(x)
+)
+df['supergenres_str'] = df['track.artists.0.supergenres'].apply(
+    lambda x: ' '.join(x)
+)
+
+df['text'] = df.apply(lambda x: 
+    f'Track: {x["track.name"]}<br>'+
+    f'Artist: {x["track.artists.0.name"]}<br>'+
+    f'Album: {x["track.album.name"]}<br>'+
+    f'Release: {x["track.album.release_date"]}<br>'+
+    # f'Genre: {x["genres_str"]}<br>'+
+    f'Super Genre: {x["supergenres_str"]}<br>'+
+    f'Preview Song: <a href="{x["track.preview_url"]}">Play</a><br>'+
+    f'Full Song: <a href="{x["track.external_urls.spotify"]}">Play</a><br>'
+    ,axis=1)
+
+df['size'] = df['track.popularity'].apply(lambda x: np.log10(x+1))
+
+#%%
 # From now on I will plot the data
+
+#%% Seaborn
+
+plt.figure()
+sns.scatterplot('umap_x', 'umap_y', data=df)
+
+plt.figure()
+sns.pairplot(df_analysis, corner=True)
+
+#%% PlotLy
+
 import plotly.graph_objects as go
 
-fig = go.Figure()
+mdl_plt='tsne'
+
+fig = lib.plotly_categorical_scatter(
+    df,
+    x=f'{mdl_plt}_x',
+    y=f'{mdl_plt}_y',
+    hue=f'clusters_{mdl_plt}',
+    size='size',
+    text='text',
+    link='track.external_urls.spotify'
+)
+fig.show()
+
+#%%
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+app.layout = html.Div([
+    dcc.Graph(figure=fig)
+])
+
+app.run_server(debug=True, use_reloader=False)
 
 # %%
