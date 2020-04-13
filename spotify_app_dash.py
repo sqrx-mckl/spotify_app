0e01430d3a6902d9e43868bcb569372535be65fc
sqrx-mckl <martinez.mickael1@gmail.com>
2020-04-13 03:24:45 +0200

    wip: fresh start with good genre cluster with new genre clustering, enhance info to check the other clustering

--- spotify_app_dash.py
+++ spotify_app_dash.py
@@ -25,96 +25,28 @@ pd.set_option('max_columns', None)
 
 #%% Load full dataset
 
-if False:
-    df_likedsong_enrich = pd.read_csv(Path('private', 'likedsongs_enrich.csv'),
-                                    index_col=0)
-    df: pd.DataFrame = df_likedsong_enrich[[
-        'added_at',
-        'track.album.id',
-        'track.album.images.0.height',
-        'track.album.images.0.url',
-        'track.album.images.0.width',
-        'track.album.name',
-        'track.album.release_date',
-        'track.album.release_date_precision',
-        'track.album.total_tracks',
-        'track.artists.0.id',
-        'track.artists.0.name',
-        'track.duration_ms',
-        'track.id',
-        'track.external_urls.spotify',
-        'track.name',
-        'track.popularity',
-        'track.preview_url',
-        'track.id.danceability',
-        'track.id.energy',
-        'track.id.key',
-        'track.id.loudness',
-        'track.id.mode',
-        'track.id.speechiness',
-        'track.id.acousticness',
-        'track.id.instrumentalness',
-        'track.id.liveness',
-        'track.id.valence',
-        'track.id.tempo',
-        'track.id.time_signature',
-        'track.artists.0.genres',
-        'track.artists.0.supergenres'
-    ]]
-else:
-    df: pd.DataFrame = pd.read_csv(Path('private', 'df_analysis.csv'),
-                                index_col=0)
+df:pd.DataFrame = pd.read_parquet(Path('private', 'df_analysis_dbscan.parquet'))
 
-    col_to_convert = ['track.artists.0.genres', 'track.artists.0.supergenres']
-    df[col_to_convert] = df[col_to_convert].applymap(yaml.safe_load)
+#%% Prepare for plot
 
-
-#%% Genre again
-
-genres = lib.EnrichArtistGenre(df['track.artists.0.id'],
-                                genre=df['track.artists.0.genres'])
-
-mask = lib.mask_outlier_iqr(genres.df_genre.sum())
-
-plt.figure
-genres.df_genre.loc[mask.low].plot(kind='box')
-plt.figure
-genres.df_genre.loc[mask.low].plot(kind='hist')
-
-genres.df_genre = genres.df_genre.iloc[:, mask]
-
-genres.cluster_genre_fit(algorithm='hdbscan')
-
-plt.figure()
-g = sns.countplot(genres.cluster_dbscan)
-g.set_title('DBscan clusters histogram')
-display(genres._get_group_list(genres.cluster_dbscan))
-
-genres.cluster_genre_transform(t=40, criterion='maxclust', verbose=True)
-genres.plot_chord_supergenre()
-
-#%%
-
-plt.figure()
-genres.df_genre.sum().plot(kind='box')
-
-plt.figure()
-genres.df_genre.sum().plot(kind='hist')
-
-plt.figure()
-genres.df_genre.sum().sort_values().plot(kind='barh')
-
-#%%
-supergenre_corr = genres.df_supergenre.corr(method=lambda x, y: max(x*y))
-sns.heatmap(supergenre_corr)
-
-# I want to know which supergenre is never alone
-genres.df_supergenre.corr(method=lambda x, y: sum(x*y))
-
-supergenres_encounter = genres.df_supergenre.agg(sum, axis=1)
-genres.df_supergenre.agg(
-    lambda x: np.mean(supergenres_encounter[x>0])
+df['genres_str'] = df['track.artists.0.genres'].apply(
+    lambda x: ' '.join(x)
 )
+df['supergenres_str'] = df['track.artists.0.supergenres'].apply(
+    lambda x: ' '.join(x)
+)
+
+df['text'] = df.apply(lambda x:
+                      f'Track: {x["track.name"]}<br>' +
+                      f'Artist: {x["track.artists.0.name"]}<br>' +
+                      f'Album: {x["track.album.name"]}<br>' +
+                      f'Release: {x["track.album.release_date"]}<br>' +
+                      # f'Genre: {x["genres_str"]}<br>'+
+                      f'Super Genre: {x["supergenres_str"]}<br>' +
+                      f'Preview Song: <a href="{x["track.preview_url"]}">Play</a><br>' +
+                      f'Full Song: <a href="{x["track.external_urls.spotify"]}">Play</a><br>', axis=1)
+
+df['size'] = df['track.popularity'].apply(lambda x: np.log10(x+1))
 
 #%% Cluster
 
@@ -159,7 +91,7 @@ for algo in ['tsne', 'umap']:
     plt.figure()
     sns.scatterplot(f'{algo}_x', f'{algo}_y',
                     data=df,
-                    hue=f'clusters_{algo}')
+                    hue=f'track.artists.0.supergenres')
     plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
 
 #%% Pairplot, quite slow
@@ -174,27 +106,6 @@ if False:
                     hue=f'clusters_{algo}')
         plt.savefig(Path('private', f'pairplot_{algo}.png'),
                     bbox_inches = 'tight')
-
-#%% Prepare for dash
-df['genres_str'] = df['track.artists.0.genres'].apply(
-    lambda x: ' '.join(x)
-)
-df['supergenres_str'] = df['track.artists.0.supergenres'].apply(
-    lambda x: ' '.join(x)
-)
-
-df['text'] = df.apply(lambda x:
-                      f'Track: {x["track.name"]}<br>' +
-                      f'Artist: {x["track.artists.0.name"]}<br>' +
-                      f'Album: {x["track.album.name"]}<br>' +
-                      f'Release: {x["track.album.release_date"]}<br>' +
-                      # f'Genre: {x["genres_str"]}<br>'+
-                      f'Super Genre: {x["supergenres_str"]}<br>' +
-                      f'Preview Song: <a href="{x["track.preview_url"]}">Play</a><br>' +
-                      f'Full Song: <a href="{x["track.external_urls.spotify"]}">Play</a><br>', axis=1)
-
-df['size'] = df['track.popularity'].apply(lambda x: np.log10(x+1))
-
 
 #%% Dash/Plotly
 
