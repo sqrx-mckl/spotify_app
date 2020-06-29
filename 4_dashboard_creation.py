09ba0327546309511aafe07e810b5ac363573c50
sqrx-mckl <martinez.mickael1@gmail.com>
2020-06-29 23:32:51 +0200

    ADD: clustering done with TSNE and UMAP

--- 4_dashboard_creation.py
+++ 4_dashboard_creation.py
@@ -45,3 +45,45 @@ app.layout = html.Div([
 ])
 
 app.run_server(debug=True, use_reloader=False)
+
+import webbrowser
+import plotly.graph_objects as go
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+init_notebook_mode(connected=True)
+
+def click_event(trace, points, state):
+    print('test')
+    display(points.customdata)
+    [webbrowser.open(point.customdata) for point in points]
+
+
+fig = go.FigureWidget()
+
+for group_name, df_group in df_top5.groupby('clusters'):
+    fig.add_trace(go.Scattergl(
+        x=df_group['proj_x'],
+        y=df_group['proj_y'],
+        name=group_name,
+        text=df_group['text'],
+        marker_size=df_group['size'],
+        customdata=df_group['preview_url'],
+        ids=df_group.index.to_list(),
+        marker=dict(
+            color=group_name,
+            colorscale='Safe',
+            line_width=0
+        )
+    ))
+    
+# Tune marker appearance and layout
+fig.update_layout(autosize=False, width=640, height=480)
+
+# upadte the size of the points based on the overall size of all traces
+fig.update_traces(
+    mode='markers',
+    hoverinfo='text',
+    marker=dict(sizeref=max(df_top5['size'])/10.)
+)
+fig.for_each_trace(
+        lambda trace: trace.on_click(click_event, append=True)
+)
